82aa81688526304a15f7e44a52313e598e412214
hawkeyexp <hawkeyexp@googlemail.com>
2017-08-10 21:20:45 +0200

    update for app v12

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 1) Put the fhem.php somewhere on your webserver with php support (must be same host as fhem).
 
-2) Set user and password for your local fhem installation inside the fhem.php.
+2) Access fhem.php one time via webserver https/example.com/fhem.php
+   it will create the file auth.php on your webserver to place login data
 
-3) Set personal security key inside the fhem.php.
+2) Set user and password for your local fhem installation inside the auth.php
 
-4) Download and configure fhem app to your lametric time.
+3) Set personal security key inside the aurh.php
+
+4) Download and configure fhem app to your lametric time
 
 Note:
 
@@ -14,9 +17,17 @@ Sample setup inside the app:
 
 Url:	http://www.sample.de or https://www.sample.de or https://www.sample.de/path_to_script_folder<br>
 Port:	optional for servers running on custom ports<br>
-Name:	Leading text for your value of device (default: FHEM)<br>
-Device:	Your fhem device to get state value from<br>
-Icon:	Icon to display (default is i8919 when left empty)<br>
-Seckey:	Your custom string which must be same inside fhem.php to get a result from script<br>
+Name:	Announce text of the app<br>
+Device:	Your fhem device to get readings from<br>
+Icon:	Icon to display with announce text (default is i8919 when left empty)<br>
+Seckey:	Your custom string which must be same inside auth.php to get a result from script<br>
+<br>
+Reading 1-4:	readings to pick from fhem device
+Icon 1-4:	icon to show with reading 1-4
+
 <br>
 Have fun!
+<br>
+NOTE: Users of first versions have to update there fhem.php to newest version and check auth.php!
+      The update is done for later easy update to only replace fhem.php without reinsert login data
+      etc.
--- fhem.php
+++ fhem.php
@@ -1,61 +1,37 @@
 <?php
-# get device to check from app
-$name=$_GET['name'];
-$device=$_GET['device'];
-$icon=$_GET['icon'];
-$seckey=$_GET['seckey'];
 
-# Failsave
-if ($name == false)
-{
-    $name="FHEM";
+if (file_exists("auth.php")){
+    include "auth.php";
+}
+else {
+    $file=fopen("auth.php","w");
+    $string="<?php\n# your personal security-code\n\$seckeycheck = \"1234567890\";\n# login for local fhem installation\n\$username = \"username\";\n\$password = \"password\";\n?>";
+    fwrite($file,$string);
+    fclose($file);
+    if (file_exists("auth.php")){
+	include "auth.php";
+    }
+    else {
+	$data='{
+	    "frames": [
+		{
+        	    "text": "File auth.php can\'t be created!",
+        	    "icon": "i8919",
+        	    "index": 0
+    		}
+	    ]
+	}';
+	print $data;
+	break;
+    }
 }
 
-if ($icon == false)
-{
-    $icon="i8919";
-}
-
-# your personal security-code
-$seckeycheck = "";
-
-# login for local fhem installation
-$username = "";
-$password = "";
-
-if ($seckey == $seckeycheck){
-
-    $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8083/fhem&cmd=%7BValue%28%22$device%22%29%7D&XHR=1");
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
-
-    if(curl_exec($ch) === false)
-    {
-	echo 'Curl error: ' . curl_error($ch);
-    }
-    $errors = curl_error($ch);
-    $result = trim(curl_exec($ch));
-    $returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-
-    # Failsave
-    if ($result == "")
-    {
-	$result="No result!";
-    }
-
+if (($username == "username") or ($password == "password")) {
     $data='{
 	"frames": [
 	    {
-        	"text": "'.$name.': '.$result.'",
-        	"icon": "'.$icon.'",
+        	"text": "Setup your login data in auth.php!",
+        	"icon": "i8919",
         	"index": 0
     	    }
 	]
@@ -63,15 +39,154 @@ if ($seckey == $seckeycheck){
     print $data;
 }
 else {
-    $data='{
-	"frames": [
-	    {
-        	"text": "Wrong or missing seckey!",
-        	"icon": "'.$icon.'",
-        	"index": 0
-    	    }
-	]
-    }';
-    print $data;
+    # get device to check from app
+    $name=$_GET['name'];
+    $device=$_GET['device'];
+    $icon=$_GET['icon'];
+    $seckey=$_GET['seckey'];
+    $reading1=$_GET['reading1'];
+    $reading2=$_GET['reading2'];
+    $reading3=$_GET['reading3'];
+    $reading4=$_GET['reading4'];
+    $icon1=$_GET['icon1'];
+    $icon2=$_GET['icon2'];
+    $icon3=$_GET['icon3'];
+    $icon4=$_GET['icon4'];
+
+    # Failsave
+    if ($name == false)
+    {
+        $name="FHEM";
+    }
+
+    if ($icon == false)
+    {
+        $icon="i8919";
+    }
+
+    if ($seckey == $seckeycheck){
+
+        $data='{
+	    "frames": [';
+
+	# first frame
+	$data=$data.'{
+        	    "text": "'.$name.'",
+        	    "icon": "'.$icon.'",
+        	    "index": 0
+    		}';
+
+	# reading1
+	if ($reading1 != ""){
+	    $ch = curl_init(); 
+	    curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8083/fhem&cmd=%7BReadingsVal%28%22$device%22,%22$reading1%22,%22%22%29%7D&XHR=1");
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+	    $reading1 = trim(curl_exec($ch));
+	    $returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	    curl_close($ch);
+	    $data=$data.',{
+		"text": "'.$reading1.'",';
+		if ($icon1 != ""){
+		    $data=$data.'"icon": "'.$icon1.'",';
+		}
+		$data=$data.'"index": 1
+	    }';
+	}
+
+	# reading2
+	if ($reading2 != ""){
+	    $ch = curl_init(); 
+	    curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8083/fhem&cmd=%7BReadingsVal%28%22$device%22,%22$reading2%22,%22%22%29%7D&XHR=1");
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+	    $reading2 = trim(curl_exec($ch));
+	    $returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	    curl_close($ch);
+	    $data=$data.',{
+		"text": "'.$reading2.'",';
+		if ($icon2 != ""){
+		    $data=$data.'"icon": "'.$icon2.'",';
+		}
+		$data=$data.'"index": 2
+	    }';
+	}
+
+	# reading3
+	if ($reading3 != ""){
+	    $ch = curl_init(); 
+	    curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8083/fhem&cmd=%7BReadingsVal%28%22$device%22,%22$reading3%22,%22%22%29%7D&XHR=1");
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+	    $reading3 = trim(curl_exec($ch));
+	    $returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	    curl_close($ch);
+	    $data=$data.',{
+		"text": "'.$reading3.'",';
+		if ($icon3 != ""){
+		    $data=$data.'"icon": "'.$icon3.'",';
+		}
+		$data=$data.'"index": 3
+	    }';
+	}
+
+	# reading4
+	if ($reading4 != ""){
+	    $ch = curl_init(); 
+	    curl_setopt($ch, CURLOPT_URL, "https://127.0.0.1:8083/fhem&cmd=%7BReadingsVal%28%22$device%22,%22$reading4%22,%22%22%29%7D&XHR=1");
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+	    $reading4 = trim(curl_exec($ch));
+	    $returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	    curl_close($ch);
+	    $data=$data.',{
+		"text": "'.$reading4.'",';
+		if ($icon4 != ""){
+		    $data=$data.'"icon": "'.$icon4.'",';
+		}
+		$data=$data.'"index": 4
+	    }';
+	}
+
+        $data=$data.'	    ]
+        }';
+	print $data;
+    }
+    else {
+	$data='{
+	    "frames": [
+		{
+        	    "text": "Wrong or missing sec key!",
+        	    "icon": "'.$icon.'",
+        	    "index": 0
+    		}
+	    ]
+	}';
+	print $data;
+    }
 }
 ?>
